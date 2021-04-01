c709546f31d03f289fb57802705d27b976b6f247
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-04-01 23:34:30 +0200

    Solve exercise "Leap"

--- leap/leap.ml
+++ leap/leap.ml
@@ -1,2 +1,14 @@
-let leap_year _ = 
-    failwith "'leap_year' is missing"
+let (%) a b =
+    let rem = a mod b in
+        if rem >= 0 then rem
+        else rem + b
+    ;;
+
+let (%%) a b = (a % b) = 0;;
+
+let (%~) a b = not (a %% b);;
+
+let leap_year y =
+    (y %% 4) && ((y %~ 100) || (y %% 400))
+    ;;
+
