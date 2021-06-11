1d8ee0ee7634dc3a46589928aae19fcab8bcaf42
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-06-11 11:00:41 +0200

    Solve exercise "Hamming" (cont.)
    Fixed some style glitches (spacing, indentation),
     and removed a couple of unnecessary parentheses.

--- hamming/hamming.ml
+++ hamming/hamming.ml
@@ -2,16 +2,16 @@ type nucleotide = A | C | G | T
 
 let hamming_distance ns_left ns_right =
   if List.length ns_left <> List.length ns_right then
-    if       List.length ns_left == 0 then
+    if      List.length ns_left  == 0 then
       Error "left strand must not be empty"
     else if List.length ns_right == 0 then
       Error "right strand must not be empty"
     else
       Error "left and right strands must be of equal length"
-   else Ok
+  else Ok
     (List.length
       (List.filter
-        (fun np -> ((fst np) <> (snd np)))
+        (fun np -> (fst np) <> (snd np))
         (List.combine ns_left ns_right)
        )
      )
